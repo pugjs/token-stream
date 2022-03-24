a8c32006a0841c010fab3abf5b48b867d68c92a3
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-03-24 11:57:54 +0800

    Add typescript declaration (.d.ts) file

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+export = TokenStream;
+
+class TokenStream<T = any> {
+  constructor(tokens: T[]);
+  lookahead(index: number): T | undefined;
+  peek(): T | undefined;
+  advance(): T | undefined;
+  defer(token: T): void
+}
